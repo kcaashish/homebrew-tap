2dbc8f5203950bed968732a96ff9907f525b49ce
Aashish KC <kcaashish@gmail.com>
2024-04-05 02:45:52 +0545

    Add versioning and livecheck (if it worked for nightly)

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,15 +1,26 @@
 cask 'neovim-nightly' do
-  version :latest
-  sha256 :no_check
+  arch arm: 'arm64', intel: 'x86_64'
 
-  url 'https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz'
+  version '0.10.0,2671,gdc110cba3'
+  sha256 arm: '1c54b349ac01d36848e2d7685473da72dbf974942620dbfdcc8015757e24aaae',
+         intel: '1c54b349ac01d36848e2d7685473da72dbf974942620dbfdcc8015757e24aa2e'
+
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz"
   name 'Neovim Nightly'
   desc 'Nightly build of the Neovim text editor'
-  homepage 'https://neovim.io/'
+  homepage 'https://github.com/neovim/neovim'
+
+  livecheck do
+    url 'https://github.com/neovim/neovim/releases/tag/nightly'
+    regex(/(?:<code>[^<>]*?v)(\d+(?:\.\d+)+)[._-]dev[._-](\d+)[+-_](\w+)(?!:[^<>]*?Build)/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match.first},#{match.second},#{match.third}" }
+    end
+  end
 
   binary 'nvim-macos/bin/nvim'
 
   postflight do
-    system_command 'xattr', args: ['-c', "#{staged_path}/nvim-macos/bin/nvim"]
+    system_command 'xattr', args: ['-c', "#{staged_path}/nvim-macos-#{arch}/bin/nvim"]
   end
 end
